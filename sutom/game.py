6ebab285606c3b9af41d7aa78ccf963358f6bd16
82L <16747176+82L@users.noreply.github.com>
2023-02-24 00:06:26 +0100

    update endgame

--- sutom/game.py
+++ sutom/game.py
@@ -2,43 +2,47 @@ import random
 
 from os import system, name
 
+MAXTURNNUMBER: int = 6
 
 from sutom.dictionnary import  words
 class Game:
-    word_to_find: str
+    _word_to_find: str
     word_length: int
     tested_words: list[list[str]]
     tested_letters: list[str]
     word_is_not_complete: bool
     number_of_turn_passed: int
-    MAXTURNNUMBER: int = 6
 
     def __init__(self):
         pass
 
     def initialize(self):
-        self.word_to_find = words[random.randint(0, len(words) - 1)]
-        self.word_length = len(self.word_to_find)
+        """Initialize the game"""
+        self._word_to_find = words[random.randint(0, len(words) - 1)]
+        self.word_length = len(self._word_to_find)
         self.number_of_turn_passed = 0
-        self._set_first_trie()
+        self._set_first_try()
         self.word_is_not_complete = True
 
 
-    def _set_first_trie(self):
-        self.tested_words = [[self.word_to_find[0]]]
+    def _set_first_try(self):
+        """Create a list based on letter number"""
+        self.tested_words = [[self._word_to_find[0]]]
         for i in range(0, self.word_length - 1):
             self.tested_words[0].append("_")
 
     def launch_game(self):
+        """Launch game"""
         self._loop()
-        self.print_tries()
+        self._finish_end_game()
 
     def _loop(self):
+        """Game loop"""
         while self.word_is_not_complete and \
-                self.number_of_turn_passed < self.MAXTURNNUMBER :
-            print(f"Turn lefts: {self.MAXTURNNUMBER-self.number_of_turn_passed}")
+                self.number_of_turn_passed < MAXTURNNUMBER :
+            print(f"Turn lefts: {MAXTURNNUMBER-self.number_of_turn_passed}")
             self.print_tries()
-            word_to_test: str = self.get_word_from_user()
+            word_to_test: str = self._get_word_from_user()
             self.tested_words.append(self._test_word(word_to_test))
             self.clear()
             self.number_of_turn_passed += 1
@@ -50,7 +54,14 @@ class Game:
             result_word = input("\nEnter a word to test : \n")
         return result_word
 
-
+    def _finish_end_game(self):
+        """Shows the infos of the end of the game"""
+        self.print_tries()
+        if self.word_is_not_complete :
+            print(f"\033[91mRaté, le mot était : {self._word_to_find}\033[00m")
+        else:
+            print(f"\033[92mBravo, réussi en {self.number_of_turn_passed} "
+                  f"coups sur {MAXTURNNUMBER}\033[00m")
 
     def print_tries(self):
         """Prints all the tries made by user"""
@@ -59,18 +70,15 @@ class Game:
 
     def clear(self):
         """clears the console"""
-        # for windows
         if name == 'nt':
             _ = system('cls')
-
-        # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
 
     def _test_word(self, word_to_test: str) -> list[str]:
         """Test the word to find correspondances"""
         result : list[str] = list(word_to_test)
-        test_list: list[bool] = self.get_test_list()
+        test_list: list[bool] = self._get_test_list()
         self._find_all_matches_same_index(word_to_test, test_list, result)
         if any(test_list):
             self._find_lesser_matches(word_to_test, test_list, result)
@@ -82,7 +90,7 @@ class Game:
     def _find_all_matches_same_index(self, word_to_test:str, test_list:list[bool], result: list[str]):
         """Find all same index matches and show it"""
         for i in range(0, self.word_length):
-            if word_to_test[i] == self.word_to_find[i]:
+            if word_to_test[i] == self._word_to_find[i]:
                 test_list[i] = False
                 result[i] = f"\033[92m{result[i]}\033[00m"
 
@@ -92,12 +100,12 @@ class Game:
             if not test_list[i]:
                 continue
             for j in range(0, self.word_length):
-                if test_list[j] and word_to_test[i] == self.word_to_find[j]:
+                if test_list[j] and word_to_test[i] == self._word_to_find[j]:
                     test_list[j] = False
                     result[i] = f"\033[93m{result[i]}\033[00m"
                     break
 
-    def get_test_list(self) -> list[bool]:
+    def _get_test_list(self) -> list[bool]:
         """Return list of bool to test word given by user """
 
         result_list: list[bool] = []
